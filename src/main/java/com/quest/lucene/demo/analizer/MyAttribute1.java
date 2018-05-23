7943c428c50b3c731f33db908fe4618ebb37d005
gansm <gansm@aijk.com>
2018-05-23 14:22:40 +0800

    origin

---
+++ src/main/java/com/quest/lucene/demo/analizer/MyAttribute1.java
@@ -0,0 +1,7 @@
+package com.quest.lucene.demo.analizer;
+
+import org.apache.lucene.util.Attribute;
+
+public interface MyAttribute1 extends Attribute {
+	String getAttr();
+}
---
+++ src/main/java/com/quest/lucene/demo/common/Constants.java
@@ -0,0 +1,8 @@
+package com.quest.lucene.demo.common;
+
+/**
+ * Created by Quest on 2018/5/18.
+ */
+public class Constants {
+    public static final String DIRECTORY_FILE_PATH = "F:\\gsm\\index_store";
+}
